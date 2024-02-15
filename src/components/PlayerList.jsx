f4efc5396f72e84854007c4799c91ce9ee0d4293
ievas <ieva.ritina@gmail.com>
2024-02-15 10:56:20 -0700

    some styling issues resolved

--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -17,44 +17,46 @@ function PlayerList({ players, teams }) {
 
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th colSpan="3">
-              <h2>Players</h2>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <h2>Name</h2>
-            </td>
-            <td>
-              <h2>Status</h2>
-            </td>
-            <td>
-              <h2>Team</h2>
-            </td>
-          </tr>
-          {players.map((player) => {
-            return (
-              <tr key={player.id} onClick={handleClick}>
-                <td>
-                  <img
-                    className="small"
-                    src={player.imageUrl}
-                    alt={player.breed}
-                  />
-                  {player.name[0].toUpperCase() + player.name.slice(1)}
-                </td>
-                <td>{player.status}</td>
-                <td>{findTeam(player.teamId)}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div style={{ overflowX: "auto" }}>
+        <table width="50%">
+          <thead>
+            <tr>
+              <th colSpan="3">
+                <h2>Players</h2>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td width="40%">
+                <h2>Name</h2>
+              </td>
+              <td width="30%">
+                <h2>Status</h2>
+              </td>
+              <td width="30%">
+                <h2>Team</h2>
+              </td>
+            </tr>
+            {players.map((player) => {
+              return (
+                <tr key={player.id} onClick={handleClick}>
+                  <td>
+                    <img
+                      className="small"
+                      src={player.imageUrl}
+                      alt={player.breed}
+                    />
+                    {player.name[0].toUpperCase() + player.name.slice(1)}
+                  </td>
+                  <td>{player.status}</td>
+                  <td>{findTeam(player.teamId)}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
